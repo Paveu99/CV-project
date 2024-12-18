6498684ec80a311f8243bd7d77d701592e30f4e9
Paweł Jarecki <paweljarecki10@gmail.com>
2024-12-18 16:03:28 +0100

    Base structure set

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+export const App = () => {
+  return <main className='cv-main'>
+    <header className='cv-header'>
+      <h1>CV Pawel Jarecki</h1>
+    </header>
+    <aside className='cv-aside'>
+      <h2>Personal Data</h2>
+      <img src="" alt="" />
+      <p>Pawel Jarecki</p>
+      <p>Developer</p>
+    </aside>
+    <section className='cv-section'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Experience</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Education</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  </main>
 }
-
-export default App
